6085c97e17d11245a1cea7180fe8a6a2b333e0ea
AugustynM <augustyn.majtyka@student.uj.edu.pl>
2023-06-05 21:17:37 +0200

    added product class

---
+++ src/main/java/com/ahr/xtaldb/Product.java
@@ -0,0 +1,10 @@
+package com.ahr.xtaldb;
+
+public class Product {
+    public String name;
+    public int id;
+    public Product(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
