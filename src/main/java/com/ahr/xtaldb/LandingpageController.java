6230b51103704a0c00b9c3f826fcb47034ae5a5f
Shadow0Bit <58443512+Shadow0Bit@users.noreply.github.com>
2023-06-05 21:07:39 +0200

    added: UI list

--- src/main/java/com/ahr/xtaldb/LandingpageController.java
+++ src/main/java/com/ahr/xtaldb/LandingpageController.java
@@ -3,13 +3,15 @@ package com.ahr.xtaldb;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class LandingpageController implements Initializable {
@@ -27,13 +29,32 @@ public class LandingpageController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        productList.setCellFactory(param -> new ListCell<String>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
 
+                if (empty || item == null) {
+                    setText("");
+                } else {
+                    setText(item);
+                }
+            }
+        });
     }
 
     public void search(ActionEvent actionEvent) {
+        LinkedList<String> products = new LinkedList<String>();
+        byte[] array = new byte[100];
+        for (int i = 0; i < 1000; i++) {
+            new Random().nextBytes(array);
+            products.add(new String(array, StandardCharsets.UTF_8));
+        }
+
+        productList.getItems().addAll(products);
     }
 
-    public void goToProfile(ActionEvent actionEvent) throws IOException {
+    public void goToProfile(ActionEvent actionEvent) throws IOException {//ty chuju XDDDDDDDD
         SceneSwitchingUtils.switchScene(actionEvent, "userprofile-view.fxml");
     }
 
